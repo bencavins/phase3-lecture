7e750d839539171c5442df3fef76d00082afdf1e
Ben Cavins <ben.cavins@flatironschool.com>
2023-02-21 13:49:54 -0500

    add dicts.py

---
+++ 02-python-data-structures/src/dicts.py
@@ -0,0 +1,21 @@
+my_dict = {}
+
+# TODO Add a key/value pair
+
+# TODO Update a value
+
+# TODO Use an invalid key
+
+# TODO Test if a key exists
+
+# TODO use .get()
+
+# TODO Delete a key
+
+# TODO get the length of a dict
+
+# TODO Merge two dicts
+
+# TODO copy a dict
+
+# TODO loop over a dict
