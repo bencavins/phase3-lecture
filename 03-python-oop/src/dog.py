5ed7d24b669d4887cfdcfe747f3c0827c5d9aea5
Ben Cavins <ben.cavins@flatironschool.com>
2023-02-22 15:10:25 -0500

    add Dog class

--- 03-python-oop/src/dog.py
+++ 03-python-oop/src/dog.py
@@ -6,22 +6,48 @@ Write a class that represents a dog. We should be able to:
 """
 
 class Dog:
+    all_dogs = []
+
     def __init__(self, name, age):  # constructor
-        print('calling constrctor')
         # variable == attribute/property
-        self.name = name
-        self.age = age
+        self._name = name
+        # self.set_age(age)
+        self._age = age
         self.breed = None
+        Dog.all_dogs.append(self)
     
     def bark(self, sound):
+        self.age = -1
         return f"{self.name} says {sound}!"
+    
+    @property
+    def name(self):
+        return self._name.title()
+    
+    def get_age(self):
+        return self._age
     
     def set_age(self, new_age):
         if (new_age > 0):
-            self.age = new_age
+            self._age = new_age
+        else:
+            raise ValueError(f'invalid age: {new_age}')
+        
+    age = property(get_age, set_age)
+    
+    def __repr__(self):
+        return f'<Dog {self.name} {self.age}>'
+    
+    @classmethod
+    def print_all_dogs(cls):
+        print(cls.all_dogs)
+    
 
 
-
-my_dog = Dog('rex', 3)
-my_dog.set_age(-1)
-print(my_dog.age)
+if __name__ == '__main__':
+    # print(add(1, 3))
+    my_dog = Dog('rex', 3)
+    my_dog.bark('woof')
+    # another_dog = Dog('gus', 10)
+    # # my_dog.age = -1  # my_dog.set_age(-1)
+    # print(Dog.all_dogs)
