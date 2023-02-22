0c7ab2ff4dd3cdaff8bac7ea70733cf73c76fc1f
Ben Cavins <ben.cavins@flatironschool.com>
2023-02-22 11:10:00 -0500

    add Dog class

--- 03-python-oop/src/dog.py
+++ 03-python-oop/src/dog.py
@@ -6,4 +6,22 @@ Write a class that represents a dog. We should be able to:
 """
 
 class Dog:
-    pass  # TODO implement
+    def __init__(self, name, age):  # constructor
+        print('calling constrctor')
+        # variable == attribute/property
+        self.name = name
+        self.age = age
+        self.breed = None
+    
+    def bark(self, sound):
+        return f"{self.name} says {sound}!"
+    
+    def set_age(self, new_age):
+        if (new_age > 0):
+            self.age = new_age
+
+
+
+my_dog = Dog('rex', 3)
+my_dog.set_age(-1)
+print(my_dog.age)
