631d666d48c4f54907898feb3396afce9a3d8776
Ben Cavins <ben.cavins@flatironschool.com>
2023-03-03 15:01:40 -0500

    add relationships 1-1

--- 07-sqlalchemy-alembic/artist.py
+++ 07-sqlalchemy-alembic/artist.py
@@ -1,5 +1,5 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy import Column, Integer, String, create_engine, func
 from sqlalchemy.orm import Session
 
 Base = declarative_base()
@@ -36,10 +36,62 @@ def add_new_obj():
     # leave the with clause, cleanup happens
 
 
-if __name__ == '__main__':
+def select():
+    """
+    If we query for the whole Artist, we will be back
+    Artist objects. If we query for a column (for 
+    example: Artist.name) we will get tuples back
+    """
+    engine = create_engine('sqlite:///chinook.db')
+    with Session(engine) as session:
+        all_artists = session.query(Artist).all()
+        print(all_artists)
+
+
+def order_by():
     engine = create_engine('sqlite:///chinook.db')
     with Session(engine) as session:
         all_artists = session.query(
             Artist
         ).order_by(Artist.name.desc()).all()
         print(all_artists)
+
+def limit():
+    engine = create_engine('sqlite:///chinook.db')
+    with Session(engine) as session:
+        result = session.query(Artist).limit(1).all()
+        print(result)
+
+def filter_by():
+    engine = create_engine('sqlite:///chinook.db')
+    with Session(engine) as session:
+        result = session.query(Artist).filter(Artist.name == 'Metallica').all()
+        print(result)
+
+def bulk_update():
+    engine = create_engine('sqlite:///chinook.db')
+    with Session(engine) as session:
+        artist = session.query(Artist).update({Artist.name: Artist.name + '!'})
+        # session.add(artist)
+        session.commit()
+
+def delete():
+    engine = create_engine('sqlite:///chinook.db')
+    with Session(engine) as session:
+        artist = session.query(Artist).filter(
+            Artist.name == 'Metallica!!!!'
+        ).first()
+        # print(artist)
+        session.delete(artist)
+        session.commit()
+
+
+if __name__ == '__main__':
+    engine = create_engine('sqlite:///chinook.db')
+    with Session(engine) as session:
+        artist = session.query(Artist).filter(
+            Artist.name == 'Metallica!'
+        ).all()
+        print(artist)
+        # session.delete(artist)
+        # session.commit()
