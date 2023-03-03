b93836e9853e292c49b520b134f83046db5b070d
Ben Cavins <ben.cavins@flatironschool.com>
2023-03-03 11:14:29 -0500

    add sqlalchemy stuff

--- 07-sqlalchemy-alembic/artist.py
+++ 07-sqlalchemy-alembic/artist.py
@@ -5,14 +5,41 @@ from sqlalchemy.orm import Session
 Base = declarative_base()
 
 class Artist(Base):
+    __tablename__ = 'artists'
+
     artistId = Column(Integer(), primary_key=True)
     name = Column(String())
 
+    def __repr__(self):
+        return f"<Artist id={self.artistId} name={self.name}>"
 
-if __name__ == '__main__':
+
+def add_new_obj():
+    # create db engine
     engine = create_engine('sqlite:///chinook.db')
+
+    # create schema
     Base.metadata.create_all(engine)
 
     # session = Session(engine)
     with Session(engine) as session:
-        pass
+        # create a new artist obj
+        my_artist = Artist(
+            name='Metallica!!!'
+        )
+        # add it to the db session
+        session.add(my_artist)
+        # commit changes
+        session.commit()
+        print(my_artist)
+
+    # leave the with clause, cleanup happens
+
+
+if __name__ == '__main__':
+    engine = create_engine('sqlite:///chinook.db')
+    with Session(engine) as session:
+        all_artists = session.query(
+            Artist
+        ).order_by(Artist.name.desc()).all()
+        print(all_artists)
