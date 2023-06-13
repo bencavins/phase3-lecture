49e3238984f5638bc1c97d3ac565273ba8d23eee
Ben Cavins <ben.cavins@flatironschool.com>
2023-06-13 11:13:00 -0400

    add dicts/lists

--- 02-python-data-structures/src/dicts.py
+++ 02-python-data-structures/src/dicts.py
@@ -1,21 +1,30 @@
 my_dict = {}
 
 # TODO Add a key/value pair
+my_dict['key'] = 'value'
 
 # TODO Update a value
+my_dict['key'] = 'new value'
 
 # TODO Use an invalid key
 
 # TODO Test if a key exists
+'key' in my_dict
 
 # TODO use .get()
 
 # TODO Delete a key
+my_dict.pop('key')
 
 # TODO get the length of a dict
+len(my_dict)
 
 # TODO Merge two dicts
+my_dict.update({'x': 'y'})
 
 # TODO copy a dict
+my_dict.copy()
 
 # TODO loop over a dict
+for key in my_dict:
+    pass
--- 02-python-data-structures/src/lists.py
+++ 02-python-data-structures/src/lists.py
@@ -1,29 +1,42 @@
 my_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
 
 # TODO Test if an element exists in `my_list`
+'a' in my_list
 
 # TODO Add element to the end
+my_list.append('h')
 
 # TODO Insert element at an index
+my_list.insert(0, 'zzz')
 
 # TODO Merge two lists together
+[1, 2, 3] + [4, 5, 6]
 
 # TODO Duplicate a list
+my_list.copy()
 
 # TODO Index lookup
 
 # TODO Slice
 
 # TODO Get length
+len(my_list)
 
 # TODO Get min/max
+min(my_list)
+max(my_list)
 
 # TODO Find index of an element
+my_list.index('a')
 
 # TODO Count number of instances of an element
+my_list.count('a')
 
 # TODO Remove element from list
+my_list.remove('a')
 
 # TODO Sorting (with an without a key)
+my_list.sort()
 
 # TODO sorted() vs .sort()
+sorted(my_list)
