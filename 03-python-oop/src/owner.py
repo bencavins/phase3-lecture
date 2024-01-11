8e20a154ecd1aa73dbaf47055109f9c8fe383630
Ben Cavins <ben.cavins@flatironschool.com>
2024-01-11 13:47:09 -0500

    add owner

---
+++ 03-python-oop/src/owner.py
@@ -0,0 +1,15 @@
+"""
+Create an Owner class that relates to Dog
+Owners can have multiple Dogs
+Dogs can have only one Owner
+
+Add these Owner methods:
+add_dog(new_dog)
+  - adds a new dog obj to the owner
+get_all_dog_names()
+  - returns a list of strings of all dog names this owner owns
+get_oldest_dog()
+  - returns the oldest dog (i.e., the Dog obj with the largest age)
+get_puppies()
+  - returns a list of Dog objects that have an age between 0-1.
+"""
