53a30cfe243f689e039d1b69fd632e2cd249c9e2
Ben Cavins <ben.cavins@flatironschool.com>
2024-03-11 11:52:31 -0400

    add loops, main

---
+++ 01-python-fundamentals/main.py
@@ -0,0 +1,20 @@
+num1 = '5'
+num2 = '6'
+
+
+def add(x, y):
+    total = x + y
+    return total
+
+result = add(int(num1), int(num2))
+# print('the result was ' + str(result))
+
+x = 5
+
+
+if x > 5 or x == 5:
+    print('greater or equal')
+elif x < 5:
+    print('lesser')
+
+print('done')
