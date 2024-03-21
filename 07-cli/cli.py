a6e65c8f92bd998a807992743d797d0048111bad
Ben Cavins <ben.cavins@flatironschool.com>
2024-03-21 11:46:59 -0400

    add cli

---
+++ 07-cli/cli.py
@@ -0,0 +1,58 @@
+import random
+import os
+
+
+def get_target():
+    target = random.randint(1, 100)
+    # print(f'target number is: {target}')
+    return target
+
+def check_input(value, target):
+    if value == target:
+        print('you win!')
+
+        answer = input('do you want to play again? (y/n) ')
+        if answer == 'y':
+            target = get_target()
+            return True  # still playing
+        else:
+            return False  # not playing anymore
+    elif value < target:
+        print('too low')
+        return True  # still playing
+    else:
+        print('too high')
+        return True  # still playing
+
+
+is_playing = True
+target = get_target()
+
+
+s = """
+      ___           ___           ___           ___           ___     
+     /\__\         /\  \         /\__\         /\__\         /\__\    
+    /:/ _/_        \:\  \       /:/ _/_       /:/ _/_       /:/ _/_   
+   /:/ /\  \        \:\  \     /:/ /\__\     /:/ /\  \     /:/ /\  \  
+  /:/ /::\  \   ___  \:\  \   /:/ /:/ _/_   /:/ /::\  \   /:/ /::\  \ 
+ /:/__\/\:\__\ /\  \  \:\__\ /:/_/:/ /\__\ /:/_/:/\:\__\ /:/_/:/\:\__\
+ \:\  \ /:/  / \:\  \ /:/  / \:\/:/ /:/  / \:\/:/ /:/  / \:\/:/ /:/  /
+  \:\  /:/  /   \:\  /:/  /   \::/_/:/  /   \::/ /:/  /   \::/ /:/  / 
+   \:\/:/  /     \:\/:/  /     \:\/:/  /     \/_/:/  /     \/_/:/  /  
+    \::/  /       \::/  /       \::/  /        /:/  /        /:/  /   
+     \/__/         \/__/         \/__/         \/__/         \/__/   
+"""
+# print(s)
+
+while is_playing:
+    os.system('clear')
+    value = input("Enter a number (type 'q' to quit): ")
+
+    if value == 'q':
+        print('user quit')
+        exit()
+
+    value = int(value)
+    is_playing = check_input(value, target)
+    
+print('done')
