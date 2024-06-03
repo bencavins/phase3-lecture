c6345b4df9a79a0377fd57dae609749b0549c888
Ben Cavins <ben.cavins@flatironschool.com>
2024-06-03 15:49:25 -0400

    add looping practice problems

---
+++ 02-python-data-structures/src/practice/looping_practice.py
@@ -0,0 +1,69 @@
+# 1. write a function that takes an array of numbers and squares each number
+def get_sqares(numbers):
+    pass
+
+# example
+# print(get_sqares([1, 2, 3, 4, 5]))
+# #=> [1, 4, 9, 16, 25]
+
+
+# 2. write a function that filters out all the odd numbers
+def get_evens(numbers):
+    pass
+
+# example
+# print(get_evens([1, 2, 3, 4, 5]))
+# #=> [2, 4]
+
+
+# 3. write a function that takes an array of dicts and returns an
+# array of the 'name' values
+def get_names(people):
+    pass
+
+test_data = [
+    {
+        'name': 'kermit',
+        'age': 44,
+    },
+    {
+        'name': 'mrs. piggy',
+        'age': 36,
+    },
+    {
+        'name': 'gonzo',
+        'age': 22
+    }
+]
+
+# example
+# print(get_names(test_data))
+# #=> ['kermit', 'mrs. piggy', 'gonzo']
+
+
+# 4. write a function that filters out all fruit that isn't red
+def get_reds(fruit):
+    pass
+
+test_data = [
+    {
+        'name': 'strawberry',
+        'color': 'red',
+    },
+    {
+        'name': 'banana',
+        'color': 'yellow',
+    },
+    {
+        'name': 'blueberry',
+        'color': 'blue',
+    },
+    {
+        'name': 'raspberry',
+        'color': 'red',
+    },
+]
+
+# example
+# print(get_reds(test_data))
+# #=> [{'name': 'strawberry', 'color': 'red'}, {'name': 'raspberry', 'color': 'red'}]
