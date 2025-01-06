6954457f0fe0df051b13c71e73f6e381e9cc98ad
Ben Cavins <bencavins@gmail.com>
2025-01-06 10:29:55 -0500

    add

---
+++ 02-python-data-structures/src/practice/string_practice.py
@@ -0,0 +1,14 @@
+def capitalize(word):
+    return ""
+
+# example
+# print(capitalize("hello"))
+# #=> "Hello"
+
+
+def format(product):
+    return ""
+
+# example
+# print(format({"name": "laptop", "brand": "hp", "price": 299.99}))
+# #=> "laptop (hp): $299.99"
